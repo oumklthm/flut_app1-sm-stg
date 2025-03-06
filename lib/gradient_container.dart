4593a3bd125cbcd74800a7f4b98fd735498eeb78
Keltoume378 <Keltoume378@gmail.com>
2025-03-06 13:32:52 +0000

    second commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/dice_roll.dart';
 import 'package:flutter_app1/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GridientContainer extends StatelessWidget {
-  const GridientContainer(this.color1, this.color2, {super.key});
-  
-  const GridientContainer.purple({super.key})
+  const  GridientContainer(this.color1, this.color2, {super.key});
+
+ const  GridientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
+ 
 
   @override
   Widget build(context) {
@@ -24,22 +26,8 @@ class GridientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child:  Center(
-        child : Column (children: [
-   Image.asset('assets/images/dice-2.png' ,
-    width: 200),
-    TextButton(
-      onPressed: (){
-//,,,,
-
-      },
-         child:  const Text(' Roll raci '),
-         ),
-
-        ],
-        ),
-      
-     
+      child: Center(
+       child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app1/gradient_container.dart';
 void main() {
   runApp(
-  const  MaterialApp(
+    const  MaterialApp(
       home: Scaffold(
           // backgroundColor: Colors.blue,
           body: GridientContainer(
-             Color.fromARGB(255, 54, 11, 128),
-              Color.fromARGB(255, 24, 7, 55),
+               Color.fromARGB(255, 54, 11, 128),
+                Color.fromARGB(255, 24, 7, 55),
           ),
           ),
     ),
